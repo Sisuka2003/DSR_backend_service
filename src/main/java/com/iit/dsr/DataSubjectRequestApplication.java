d6f6feb4827f51efd95179bbb33ea72a9b0b61b0
Sisuka Weerasinghe <sisukaweerasinghe@gmail.com>
2025-10-12 22:37:32 +0530

    [DSR - MSc FYP]Initial Commit

---
+++ src/main/java/com/iit/dsr/DataSubjectRequestApplication.java
@@ -0,0 +1,13 @@
+package com.iit.dsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSubjectRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSubjectRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iit/dsr/DataSubjectRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iit.dsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSubjectRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
