c4b14f60fc4c1df05e23b8ef459286d3ada33eaa
Sisuka Weerasinghe <sisukaweerasinghe@gmail.com>
2025-12-24 19:34:49 +0530

    [DSR - MSc FYP] added cross origin to integrate with the FE service

--- src/main/java/com/iit/dsr/controller/CommonController.java
+++ src/main/java/com/iit/dsr/controller/CommonController.java
@@ -3,12 +3,14 @@ package com.iit.dsr.controller;
 import com.iit.dsr.service.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "app/v1/commons")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CommonController {
 
     @Autowired
--- src/main/java/com/iit/dsr/controller/DataSubjectOperationsController.java
+++ src/main/java/com/iit/dsr/controller/DataSubjectOperationsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/operations/datasubject")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DataSubjectOperationsController {
 
     @Autowired
