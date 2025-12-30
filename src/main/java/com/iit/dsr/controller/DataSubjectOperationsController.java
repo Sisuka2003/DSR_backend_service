31473d00969a0547fd015f33f22258f9089dd504
Sisuka Weerasinghe <sisukaweerasinghe@gmail.com>
2025-12-30 18:43:32 +0530

    Fixed issues with the repos when integrating with the frontend

---
+++ src/main/java/com/iit/dsr/controller/DataSubjectOperationsController.java
@@ -0,0 +1,44 @@
+package com.iit.dsr.controller;
+
+import com.iit.dsr.dto.requests.operations.DataSubjectOperationsRequestDto;
+import com.iit.dsr.service.DataSubjectOperationsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/operations/datasubject")
+@CrossOrigin(origins = "http://localhost:3000")
+@Log4j2
+public class DataSubjectOperationsController {
+
+    @Autowired
+    private DataSubjectOperationsService dataSubjectOperationsService;
+
+    @PostMapping(value = "/requestDataSubjectData")
+    public ResponseEntity<?> requestOrganizationData(@RequestBody DataSubjectOperationsRequestDto requestDto){
+        return dataSubjectOperationsService.getStoredData(requestDto);
+    }
+
+    @PutMapping(value = "/modifyDataSubjectData")
+    public ResponseEntity<?> requestForDataCorrection(@RequestBody DataSubjectOperationsRequestDto requestDto){
+            log.info("DataSubjectOperationsController => requestForDataCorrection => Invoked with :"+requestDto);
+            return dataSubjectOperationsService.updatedStoredData(requestDto);
+    }
+
+    @DeleteMapping(value = "/deleteDataSubjectData")
+    public ResponseEntity<?> requestToDeleteData(@RequestBody DataSubjectOperationsRequestDto requestDto){
+        log.info("DataSubjectOperationsController => requestToDeleteData => Invoked with :"+requestDto);
+        return dataSubjectOperationsService.deleteStoredData(requestDto);
+    }
+
+    @PostMapping(value = "/generateDsrReport")
+    public ResponseEntity<?> requestToExportData(@RequestBody DataSubjectOperationsRequestDto requestDto){
+        return dataSubjectOperationsService.generateDataExportReport(requestDto);
+    }
+
+    public ResponseEntity<?> requestWithdrawConsent(@RequestBody DataSubjectOperationsRequestDto requestDto){
+        return null;
+    }
+}
